b9532e24e93f6c1b040a62d166e08ca79941e871
Peter Olson <peter@overtype.com>
2018-05-30 22:38:10 -0400

    Update Bio.js

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -12,7 +12,8 @@ const Bio = () => (
     <Venn />
     
     <h3>Where I'm at. Where I've been.</h3>
-    <p>I’m currently a director and creative technologist at <a href="https://www.ideo.com" target="_blank" rel="noopener noreferrer">IDEO</a>, where I use design and technology to launch new products and change big systems.</p>
+    <p>I’m currently a director of technology for content systems and APIs at <a href="http://nbcnews.com/" target="_blank" rel="noopener noreferrer">NBC News Digital</a>.</p>
+    <p>In the past I was a director and creative technologist at <a href="https://www.ideo.com" target="_blank" rel="noopener noreferrer">IDEO</a>, focusing on the intersection of design and technology to launch new products and change big systems.</p>
     <p>I helped start the Digital Media Group at <a href=
     "https://marvel.com"  target="_blank" rel="noopener noreferrer">Marvel Entertainment</a> and created things like the first digital comics program backed by the comic industry, the first developer program in the comics industry, and used graph theory to understand the complexities of the Marvel Universe.</p>
 	</div>
