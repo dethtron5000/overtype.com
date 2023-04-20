3e5e9ee237cfe7cf89fc1c2e23e19ef45d35e2d2
Peter Olson <olsopete@amazon.com>
2023-04-20 13:58:14 -0400

    Fixing tests

--- src/pages/Main.test.js
+++ src/pages/Main.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { createRoot } from 'react-dom/client';
 import Main from './Main';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
+  const container = document.createElement('div');
+
+  const root = createRoot(container);
+  root.render(<Main />);
 });
